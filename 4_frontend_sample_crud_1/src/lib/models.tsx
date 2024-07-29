04310aa591c461b63c6a22d6e6ea24188678a744
PichayaPhatcharoen <65070155@kmitl.ac.th>
2024-07-30 01:09:46 +0700

    update lib model

--- 4_frontend_sample_crud_1/src/lib/models.tsx
+++ 4_frontend_sample_crud_1/src/lib/models.tsx
@@ -3,5 +3,11 @@ export interface Book {
   title: string;
   author: string;
   year: number;
+  description: string;
+  prologue: string;
+  type1: string;
+  type2: string;
+  type3: string;
+  type4: string;
   is_published: boolean;
 }
