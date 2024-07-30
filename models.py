91a1c8ac779bc88b1032d152ccdfc63f3d450352
PichayaPhatcharoen <65070155@kmitl.ac.th>
2024-07-30 18:09:36 +0700

    models update

--- models.py
+++ models.py
@@ -18,3 +18,19 @@ class Book(Base):
     type4 = Column(String, index=True)
     is_published = Column(Boolean, index=True)
 
+class Menu(Base):
+    __tablename__ = 'menus'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(float, index=True)
+    description = Column(String, index=True)
+
+class CustomerOrder(Base):
+    __tablename__ = 'customer_orders'
+
+    order_id = Column(Integer, primary_key=True, index=True)
+    customer_name = Column(String, index=True)
+    order_note = Column(String, index=True)
+    quantity = Column(Integer, index=True)
+    total_price = Column(float, index=True)
